fe3ac9308b3fa455bb42db751634d811b7cad60d
sossyyyy <davide.bruna170706@gmail.com>
2026-04-28 10:19:11 +0200

    Gameboard and moves implementation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.printBoard();
+        gameBoard.moveUP(); // same as gameboard.move(1)
+        gameBoard.printBoard();
+        gameBoard.moveRIGHT(); // same as gameboard.move(2)
+        gameBoard.printBoard();
+        gameBoard.moveDOWN(); // same as gameboard.move(3)
+        gameBoard.printBoard();
+        gameBoard.moveLEFT(); // same as gameboard.move(4)
+        gameBoard.printBoard();
+    }
 }
---
+++ src/Moves.java
@@ -0,0 +1,7 @@
+public interface Moves {
+    public int[] move(int m);
+    public int[] moveUP();
+    public int[] moveDOWN();
+    public int[] moveLEFT();
+    public int[] moveRIGHT();
+}
