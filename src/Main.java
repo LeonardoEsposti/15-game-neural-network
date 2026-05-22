cb0fae1df20e3025013e3d1f007f86cba09eefa0
Antonio Sorrentino <antosorre13@gmail.com>
2026-05-22 21:20:50 +0200

    now the board is an array and not a matrix

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import game.GameBoard;
 
 public class Main {
     public static void main(String[] args) {
+        GameBoard prova = new GameBoard();
+        System.out.println(prova.hashCode());
 
     }
 }
