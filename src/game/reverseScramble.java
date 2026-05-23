074e5140bb7b40c0bd0dfca3e7b82a6316ad5e85
Antonio Sorrentino <antosorre13@gmail.com>
2026-05-23 11:47:48 +0200

    adding reverse scrambling

---
+++ src/game/reverseScramble.java
@@ -0,0 +1,19 @@
+package game;
+import dataStructures.queue;
+import java.util.HashMap;
+
+public class reverseScramble {
+    HashMap<GameBoard, Integer> distanze = new HashMap<>();
+    queue coda = new queue();
+
+    public HashMap calculate(int ceiling, GameBoard father ){
+        coda.add(father)
+        int distanza = 0
+        while (distanza < ceiling){
+            while(coda.)
+
+
+        }
+    }
+
+}
