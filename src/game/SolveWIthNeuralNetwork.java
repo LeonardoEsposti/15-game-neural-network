aed7436b2a21ee7872f805d509a5df5a5bec8d07
sossyyyy <davide.bruna170706@gmail.com>
2026-06-30 17:11:30 +0200

    fixato il metodo per il SolveWIthNeuralNetwork

--- src/game/SolveWIthNeuralNetwork.java
+++ src/game/SolveWIthNeuralNetwork.java
@@ -3,26 +3,45 @@ package game;
 import dataStructures.Matrix;
 import dataStructures.Queue;
 import exceptions.EmptyQueueException;
-import neuralNetwork.*;
+import neuralNetwork.NeuralNetwork;
 
 public class SolveWIthNeuralNetwork {
     public static void solve(GameBoard board) throws EmptyQueueException {
         NeuralNetwork neuralNetwork = new NeuralNetwork();
-        double prediction= neuralNetwork.predict(new Matrix(board)).getFirstEntry();
         Queue children;
-        GameBoard closestChild=board;
-        while(!closestChild.isComplete()){
-            children= closestChild.Children();
-            for (int i=0;i<4;i++) { //AT MOST 4 TIMES
+        GameBoard parentBoard = board;
+
+        while (!parentBoard.isComplete()) {
+            double minPrediction = Double.MAX_VALUE;
+            int smallestMove = 0;
+            GameBoard smallestBoard = null;
+
+            children = parentBoard.Children();
+
+            while (children.isNotEmpty()) {
                 int moved = children.readMove();
                 GameBoard child = children.get();
-                if (neuralNetwork.predict(new Matrix(child)).getFirstEntry() < prediction) {
-                    System.out.println("Next move is " + moved);
-                    child.printBoard();
-                    closestChild = child;
-                    break;
+                double childPrediction = neuralNetwork.predict(new Matrix(child)).getFirstEntry();
+
+                if (childPrediction < minPrediction) {
+                    minPrediction = childPrediction;
+                    smallestMove = moved;
+                    smallestBoard = child;
                 }
             }
+            System.out.println("Next move is " + getMove(smallestMove));
+            smallestBoard.printBoard();
+            parentBoard = smallestBoard;
         }
+    }
+
+    public static String getMove(int move) {
+        return switch (move) {
+            case 1 -> "UP";
+            case 2 -> "LEFT";
+            case 3 -> "DOWN";
+            case 4 -> "RIGHT";
+            default -> "ERROR";
+        };
     }
 }
