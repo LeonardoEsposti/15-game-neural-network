744bdb8a9fe95cfa2fb679a1cc757889150468f3
sossyyy <davide.bruna170706@gmail.com>
2026-07-09 22:14:46 +0200

    Definitive final version - overwriting remote

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import dataStructures.Matrix;
 import exceptions.EmptyQueueException;
 import game.GameBoard;
 import game.IDA_Solver;
@@ -16,7 +17,7 @@ public class Main {
         // TRAINING NEURAL NETWORK ON DATASET
         //NeuralNetwork nn = new NeuralNetwork();
         //Trainer.trainFromDataset("src/training/dataset.csv", nn);
-        //System.out.println(nn.predict(new Matrix(new GameBoard(new int[]{1,2,3,4,5,6,7,8,9,10,0,11,13,14,15,12}))).getFirstEntry());
+        //System.out.println(nn.predict(new Matrix(new GameBoard(new int[]{0,6,3,4,7,9,1,8,5,10,2,11,13,14,15,12}))).getFirstEntry());
 
         GameBoard board= new GameBoard(new int[]{1,6,2,10,5,0,4,3,9,13,7,8,12,14,15,11});
         NeuralNetwork nn= new NeuralNetwork();
